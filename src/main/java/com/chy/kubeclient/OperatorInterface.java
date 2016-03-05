19339158d36ae152e20a623c4c216eaba696de0d
chyzhao <1440920762@qq.com>
2016-03-05 20:35:42 +0800

    add delete function

--- src/main/java/com/chy/kubeclient/OperatorInterface.java
+++ src/main/java/com/chy/kubeclient/OperatorInterface.java
@@ -8,12 +8,18 @@ public interface OperatorInterface {
     public String getAllNSs();
     public String getAllRCsInNS(String ns);
     public String getAllSrvsInNS(String ns);
+
     public String createNS(String json);
     public String createRCInNS(String json, String ns);
     public String createSrvInNS(String json, String ns);
     public String createPodInNS(String json, String ns);
+
     public String getPodInfoByName(String podName, String ns);
     public String getRCInfoByName(String rcName, String ns);
     public String getSrvInfoByName(String srvName, String ns);
     public String getNodeInfoByName(String nodeName);
+
+    public String deletePodInNS(String ns, String podName);
+    public String deleteRCInNS(String ns, String rcName);
+    public String deleteSrvInNS(String ns, String srvName);
 }
